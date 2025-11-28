4c563cb3411ddb5a606e7de6942fd5540dd876f8
joshuar-dd <joshuavr1396@gmail.com>
2025-11-27 20:07:22 -0600

    Primera parte

--- Proyecto/Proyecto.cpp
+++ Proyecto/Proyecto.cpp
@@ -1,19 +1,22 @@
-// Proyecto.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello word!\n";
-}
+struct Contacto {
+    string nombre;
+    string telefono;
+    string email;
+    Contacto* siguiente;
+};
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+struct NodoArbol {
+    Contacto* contacto;
+    NodoArbol* izquierdo;
+    NodoArbol* derecho;
+};
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+struct Operacion {
+    string tipo;
+    string contacto;
+    Operacion* siguiente;
+};
