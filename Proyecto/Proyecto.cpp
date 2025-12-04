d134ab0d93e27950fef852a66a8b435f3f65517f
joshuar-dd <joshuavr1396@gmail.com>
2025-12-04 09:58:15 -0600

    Tercera parte

--- Proyecto/Proyecto.cpp
+++ Proyecto/Proyecto.cpp
@@ -40,3 +40,59 @@ void pausar() {
     cin.ignore();
     cin.get();
 }
+
+void agregarContacto() {
+    if (totalContactos >= MAX_CONTACTOS) {
+        cout << "No se pueden agregar mas contactos\n";
+        return;
+    }
+
+    Contacto* nuevo = new Contacto;
+    cout << "Nombre: ";
+    cin.ignore();
+    getline(cin, nuevo->nombre);
+    cout << "Telefono: ";
+    getline(cin, nuevo->telefono);
+    cout << "Email: ";
+    getline(cin, nuevo->email);
+    nuevo->siguiente = NULL;
+
+    contactos[totalContactos] = nuevo;
+    totalContactos++;
+
+    Operacion* nuevaOp = new Operacion;
+    nuevaOp->tipo = "AGREGAR";
+    nuevaOp->contacto = nuevo->nombre;
+    nuevaOp->siguiente = pilaHistorial;
+    pilaHistorial = nuevaOp;
+
+    cout << "Contacto agregado exitosamente\n";
+}
+
+void ordenarContactos() {
+    for (int i = 0; i < totalContactos - 1; i++) {
+        for (int j = 0; j < totalContactos - i - 1; j++) {
+            if (contactos[j]->nombre > contactos[j + 1]->nombre) {
+                Contacto* temp = contactos[j];
+                contactos[j] = contactos[j + 1];
+                contactos[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void listaContactos() {
+    if (totalContactos == 0) {
+        cout << "No hay contactos registrados\n";
+        return;
+    }
+
+    ordenarContactos();
+
+    cout << "LISTA DE CONTACTOS  \n";
+    for (int i = 0; i < totalContactos; i++) {
+        cout << i + 1 << contactos[i]->nombre
+            << " | " << contactos[i]->telefono
+            << " | " << contactos[i]->email << endl;
+    }
+}
